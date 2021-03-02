2cdf68165fecd9176285f6107e0d4d914fde492b
fruity nik <nikandr33@inbox.ru>
2021-03-02 16:00:02 +0300

    hotfix

--- upload/catalog/controller/extension/module/product_display_nik.php
+++ upload/catalog/controller/extension/module/product_display_nik.php
@@ -9,6 +9,10 @@ class ControllerExtensionModuleProductDisplayNik extends Controller {
 
 		$this->load->model('tool/image');
 
+        $this->document->addStyle('catalog/view/javascript/jquery/swiper/css/swiper.min.css');
+        $this->document->addStyle('catalog/view/javascript/jquery/swiper/css/opencart.css');
+        $this->document->addScript('catalog/view/javascript/jquery/swiper/js/swiper.jquery.js');
+
 		$data['random_products'] = array();
 
 		$data = $setting;
